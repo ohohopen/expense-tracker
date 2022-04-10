31c897628e5cb5e5b4a3d5527abce844e5612682
ohohopen <ohohopen@gmail.com>
2022-04-10 09:38:17 +0800

    RESTful 風格調整

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const db = require("./config/mongoose");
 const port = process.env.PORT;
 const exphbs = require("express-handlebars");
 const router = require("./routes");
+const methodOveride = require("method-override");
+app.use(methodOveride("_method"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 app.engine("hbs", exphbs({ defaultLayout: "main", extname: "hbs" }));
