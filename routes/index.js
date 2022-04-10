e8f432d60b0a256e6e242fdacee3dcd7f59196ac
ohohopen <ohohopen@gmail.com>
2022-04-10 21:13:56 +0800

    Facebook第三方登入

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require("express");
 const router = express.Router();
+
 const home = require("./modules/home");
 const records = require("./modules/records");
 const users = require("./modules/users");
+const auth = require("./modules/auth");
 const { authenticator } = require("../middleaware/auth");
+
 router.use("/records", authenticator, records);
 router.use("/users", users);
+router.use("/auth", auth);
 router.use("/", authenticator, home);
+
 module.exports = router;
